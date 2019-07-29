ea8c26f9fe81bb0fbb6030b478f324acfbde3848
pychthonic <fridgewolf@protonmail.com>
2019-07-28 21:45:49 -0400

    changed config to use file in /etc folder instead of environment variables

--- mammothflaskblog/config.py
+++ mammothflaskblog/config.py
@@ -1,18 +1,23 @@
-import os
+import json
 
 """
-For full functionality, set the following environment variables correctly in your operating system.
+For full functionality, create a json file in your etc folder 
+called 'mfb_config.json with the following key/value pairs:
 """
+
+
+with open('/etc/mfb_config.json') as config_file:
+    config = json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['fake_email@gmail.com']
-    MFB_EMAIL = "fake_email@gmail.com"
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = config.get('MAIL_SERVER')
+    MAIL_PORT = int(config.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = config.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD = config.get('MAIL_PASSWORD')
+    ADMINS = ['mammothflaskblog@gmail.com']
+    MFB_EMAIL = "mammothflaskblog@gmail.com"
     SITE_NAME = "MAMMOTH FLASK BLOG"
